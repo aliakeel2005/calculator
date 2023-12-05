cf093f82436fa4660e7d10aab1afd8ba84fd7f25
aliakeel2005 <ali.akeelqq123@gmail.com>
2023-12-05 15:06:44 +0300

    first method of working the numbers

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function divide(num1,num2) {
 let num1 = null
 let num2 = null
 let opr = null
-
+let displayValue = null
 function operate(num1, opr, num2) {
     if(opr === '+') { return add(num1,num2)}
     if(opr === '-') { return subtract(num1,num2)}
@@ -25,8 +25,17 @@ function operate(num1, opr, num2) {
     if(opr === '/') { return divide(num1,num2)}
 }
 
-const numbers = document.querySelector('.numbers')
-let displayValue = null
-numbers.addEventListener('click', () => { const pressedButton = event.target
-    displayValue = pressedButton.textContent})
-console.log(displayValue)
+const buttons = document.querySelector('.buttons')
+const equals = document.querySelector('.equals')
+buttons.addEventListener('click', () => { 
+    const pressedButton = event.target
+    const buttonValue = pressedButton.textContent
+    if(Number.isInteger(parseInt(buttonValue))){
+   if(displayValue === null) {num1 = buttonValue}
+   else {num2 = buttonValue}
+displayValue = buttonValue }
+else {opr = buttonValue}})
+
+
+console.log(num1)
+console.log(num2)
