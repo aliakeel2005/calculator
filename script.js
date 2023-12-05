5b624353fa7fa9e87c9daa7dbe7d958afe806521
aliakeel2005 <ali.akeelqq123@gmail.com>
2023-12-05 13:57:33 +0300

    version one of operate

---
+++ script.js
@@ -0,0 +1,28 @@
+function add(num1,num2) {
+    return num1 + num2
+}
+
+function subtract(num1,num2) {
+    return num1 - num2
+}
+
+function multiply(num1,num2) {
+    return num1 * num2
+}
+
+function divide(num1,num2) {
+    return num1 / num2
+}
+
+let num1 = null
+let num2 = null
+let opr = null
+
+function operate(num1, opr, num2) {
+    if(opr === '+') { return add(num1,num2)}
+    if(opr === '-') { return subtract(num1,num2)}
+    if(opr === '*') { return multiply(num1,num2)}
+    if(opr === '/') { return divide(num1,num2)}
+}
+
+console.log(operate(2 ,'+' ,8))
